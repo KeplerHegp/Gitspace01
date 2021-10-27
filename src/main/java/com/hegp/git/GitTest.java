8004f5fa3607a3694da6920ece7c9071d9f8174d
hegp <hegp@asiainfo.com>
2021-10-27 10:39:47 +0800

    Second commit

--- src/main/java/com/hegp/git/GitTest.java
+++ src/main/java/com/hegp/git/GitTest.java
@@ -2,6 +2,7 @@ package com.hegp.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git01!");
+        System.out.println("Hello Git02!");
     }
 }
